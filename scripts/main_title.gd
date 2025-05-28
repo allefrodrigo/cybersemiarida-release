9ffa608a8313c9f6c4609848acb9f8d67b899f23
Allef Windows <dev.allefschmidt@gmail.com>
2025-05-27 21:20:09 -0300

    new dungeon

--- scripts/main_title.gd
+++ scripts/main_title.gd
@@ -13,3 +13,7 @@ func _on_texture_button_pressed() -> void:
 func _on_fade_timer_timeout() -> void:
 	# Pausa a música
 	get_tree().change_scene_to_file("res://levels/release/caf_01.tscn")
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventJoypadButton and event.pressed and event.button_index == JOY_BUTTON_START:
+		_on_texture_button_pressed()
